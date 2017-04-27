575baaf0f309aeec3bcf57b3a2bf6bf038f995b2
Jakob Culebro Reyes <jakob.culebro@gmail.com>
2017-04-26 20:11:42 -0500

    reduced particle total number for runtime limit

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -20,7 +20,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
 
-	if ( num_particles == 0) num_particles = 400;
+	if ( num_particles == 0) num_particles = 200;
 
 	particles.clear();
 	particles.resize(num_particles);
